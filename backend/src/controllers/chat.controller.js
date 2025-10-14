f7bbaf21a1de0a6af657396e05d8d1a406d1000b
Ojesh Mundale <ojeshmundale@gmail.com>
2025-10-14 11:37:39 +0530

    first commit

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,16 @@
+import { generateStreamToken } from "../lib/stream.js";
+
+export async function getStreamToken(req, res) {
+  try {
+    const token = generateStreamToken(req.user.id);
+
+    if (!token) {
+      return res.status(500).json({ message: "Failed to generate stream token" });
+    }
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log("Error in getStreamToken controller:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
